88137449fa80603d46e0cef5bc1b5aa585e8ca7c
parkjihye <166773498+WISDOMPARK0909@users.noreply.github.com>
2024-11-26 13:41:21 +0900

    Update TodoController.java 사번리턴

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -10,4 +10,9 @@ public class TodoController {
     public String home() {
         return "todoList";
     }
+
+    @GetMapping("/api/v1/user")
+    public String getUserId(@RequestParam String userId) {
+        return "UserId: " + userId;
+    }
 }
