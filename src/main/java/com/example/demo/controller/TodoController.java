9fef63f225b87d5e5a14d34d690df806c605c732
parkjihye <166773498+WISDOMPARK0909@users.noreply.github.com>
2024-11-26 14:09:46 +0900

    Update TodoController.java

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -14,7 +14,7 @@ public class TodoController {
     }
 
     @GetMapping("/api/v1/user")
-    public String getUserView(@RequestParam(value = "id", required = false, defaultValue = "82058454") String id, Model model) {
+    public String getUserView(@RequestParam(value = "userId", required = false, defaultValue = "82058454") String id, Model model) {
     model.addAttribute("userId", id);
     return "user"; // Thymeleaf 템플릿 이름 (user.html)
 }
